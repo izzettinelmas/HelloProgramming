7a212a4d9ca11b8f5c3d611345168f9e7803a6d4
izzettinelmas <izzettin.elmas@bil.omu.edu.tr>
2016-12-29 22:07:39 +0200

    asdaisşld
    işdslfaişsld

---
+++ CSharp/1-Hello World/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorlda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("İlk konsol uygulamam.");
+            Console.ReadKey();
+        }
+    }
+}
